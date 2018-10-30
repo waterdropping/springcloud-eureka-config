8358d1f9a199f5333ee8801910e6043ef79b9bcf
waterdropping <striveallin@gmail.com>
2018-10-30 23:42:39 +0800

    chagne from hard-coding config-server url to use config-server's service id.

--- SpringCloud-config-client/src/main/java/example/main/Application.java
+++ SpringCloud-config-client/src/main/java/example/main/Application.java
@@ -1,10 +1,12 @@
 package example.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("example")
+@EnableDiscoveryClient
 public class Application {
 
 	public static void main(String[] args) {
